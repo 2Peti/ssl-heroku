288b6c5cb01bde497266f63248d8d129e45b902a
2Peti <pney0807@gmail.com>
2020-09-27 06:06:07 +0200

    Update server.js

--- server.js
+++ server.js
@@ -5,6 +5,13 @@ var port = process.env.PORT || 8080;
 
 app.use(express.static(__dirname + '/public'));
 
+app.get('*', function(req,res,next) {
+  if(req.headers['x-forwarded-proto'] != 'https' && process.env.NODE_ENV === 'production')
+    res.redirect('https://cochces.herokuapp.com/' + req.url)
+  else
+    next() /* Continue to other routes if we're not redirecting */
+});
+
 app.get('/', function(req, res) {
 	res.redirect('https://cochces.herokuapp.com/' + req.url);
 })

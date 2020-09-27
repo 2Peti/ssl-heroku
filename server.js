14743ac111883a31ac32e2cee300bcdf5855a98e
2Peti <pney0807@gmail.com>
2020-09-27 06:03:36 +0200

    Update server.js

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ var port = process.env.PORT || 8080;
 app.use(express.static(__dirname + '/public'));
 
 app.get('/', function(req, res) {
-	res.redirect('https://cochces.herokuapp.com'+req.url);
+	res.redirect('https://cochces.herokuapp.com/' + req.url);
 })
 
 app.listen(port, function() {

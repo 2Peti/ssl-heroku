6104ebc2598559c040d22600f9283c9d5b36fcf3
2Peti <pney0807@gmail.com>
2020-09-27 06:10:06 +0200

    Update server.js

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ app.use(express.static(__dirname + '/public'));
 
 app.get('*', function(req,res,next) {
   if(req.headers['x-forwarded-proto'] != 'https' && process.env.NODE_ENV === 'production')
-    res.redirect('https://cochces.herokuapp.com:443/' + req.url)
+    res.redirect('http://cochces.herokuapp.com:443/' + req.url)
   else
     next() /* Continue to other routes if we're not redirecting */
 });

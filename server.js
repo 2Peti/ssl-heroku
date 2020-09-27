9d76052b14a8dbb0e406c61d554d99a38a12cf11
2Peti <pney0807@gmail.com>
2020-09-27 06:11:10 +0200

    Update server.js

--- server.js
+++ server.js
@@ -7,13 +7,13 @@ app.use(express.static(__dirname + '/public'));
 
 app.get('*', function(req,res,next) {
   if(req.headers['x-forwarded-proto'] != 'https' && process.env.NODE_ENV === 'production')
-    res.redirect('http://cochces.herokuapp.com:443/' + req.url)
+    res.redirect('http://cochces.herokuapp.com:443' + req.url)
   else
     next() /* Continue to other routes if we're not redirecting */
 });
 
 app.get('/', function(req, res) {
-	res.redirect('https://cochces.herokuapp.com:443/' + req.url);
+	res.redirect('https://cochces.herokuapp.com:443' + req.url);
 })
 
 app.listen(port, function() {
